2b0a84bf7ad1d705cb09cf1b727677165642af6e
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-11-30 17:58:15 +0100

    Register Form

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  firstName = '';
+  lastName = '';
+  email = '';
+  password = '';
+  passwordConfirm = '';
 
+  submit(): void {
+    console.log({
+      first_name: this.firstName,
+      last_name: this.lastName,
+      email: this.email,
+      password: this.password,
+      password_confirm: this.passwordConfirm
+    });
+  }
 }
