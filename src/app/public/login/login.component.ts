21a4f673da3d870a536adb6ae91588b7f27494a3
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-12-02 17:09:39 +0100

    Login

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { environment } from 'src/environment/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css', './../public.component.css'],
 })
 export class LoginComponent {
+  form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: '',
+    });
+  }
+
+  submit(): void {
+    this.http
+      .post(`${environment.api}/login`, this.form.getRawValue(), {
+        withCredentials: true,
+      })
+      .subscribe(() => this.router.navigate(['/']));
+  }
 }
