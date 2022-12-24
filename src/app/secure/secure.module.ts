dd3d21c0e36f99fa7b731e9ca31645817798a4e6
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-12-24 15:07:13 +0100

    Profile 01

--- src/app/secure/secure.module.ts
+++ src/app/secure/secure.module.ts
@@ -4,6 +4,8 @@ import { NavComponent } from './nav/nav.component';
 import { MenuComponent } from './menu/menu.component';
 import { SecureComponent } from './secure.component';
 import { RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
@@ -11,7 +13,9 @@ import { RouterModule } from '@angular/router';
   declarations: [
     NavComponent,
     MenuComponent,
-    SecureComponent
+    SecureComponent,
+    ProfileComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
