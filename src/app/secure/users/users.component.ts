6f47b8a7a98cc7deb320e1b1f11a6991857f5fc9
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-01-05 10:13:03 +0100

    Users table

--- src/app/secure/users/users.component.ts
+++ src/app/secure/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+  users: User[] = [];
+
+  constructor(private userService: UserService) {
+    this.userService.all().subscribe(
+      (res: any) => {
+        this.users = res.data;
+      }
+    );
+  }
 
 }
