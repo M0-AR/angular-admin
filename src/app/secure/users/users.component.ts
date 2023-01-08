0d1412221b14cd672e488bfea9829c57359cf8bb
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-01-08 08:33:55 +0100

    Deleting Users

--- src/app/secure/users/users.component.ts
+++ src/app/secure/users/users.component.ts
@@ -34,4 +34,14 @@ export class UsersComponent {
     this.page--;
     this.load();
   }
+
+  delete(id: number): void {
+    if (confirm('Are you sure you want to delete this record?')) {
+      this.userService.delete(id).subscribe(
+        () => {
+          this.users = this.users.filter(u => u.id !== id);
+        }
+      );
+    }
+  }
 }
