2fefb19a1f11e2cc8fb6d0f7ee620becab126f86
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-01-11 12:50:50 +0100

    Creating Users

--- src/app/secure/users/user-create/user-create.component.ts
+++ src/app/secure/users/user-create/user-create.component.ts
@@ -1,21 +1,43 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder} from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Role } from 'src/app/interfaces/role';
+import { RoleService } from 'src/app/services/role.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
-  styleUrls: ['./user-create.component.css']
+  styleUrls: ['./user-create.component.css'],
 })
 export class UserCreateComponent {
   form: FormGroup;
+  roles: Role[] = [];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private roleService: RoleService,
+    private userService: UserService,
+    private router: Router
+  ) {
     this.form = this.formBuilder.group({
       first_name: '',
       last_name: '',
       email: '',
-      role_id: ''
+      role_id: '',
     });
+
+    this.roleService.all().subscribe(
+      roles => this.roles = roles 
+    );
   }
+
+  submit(): void {
+    
+    this.userService.create(this.form.getRawValue()).subscribe(
+      () => this.router.navigate(['/users'])
+    );
+  }
+
 
 }
