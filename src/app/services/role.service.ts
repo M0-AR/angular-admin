abe1407452ceb28fdd4feec85d52be4a26a4a8ec
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-01-15 15:34:27 +0100

    Add Abstract Class

--- src/app/services/role.service.ts
+++ src/app/services/role.service.ts
@@ -1,17 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from 'src/environment/environment';
+import { RestService } from './rest.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class RoleService {
-  endpoint = `${environment.api}/roles`
-
-  constructor(private http: HttpClient) { }
-
-  all(): Observable<any> {
-    return this.http.get<any>(this.endpoint);
-  }
+export class RoleService extends RestService {
+  endpoint = `${environment.api}/roles`;
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,34 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from 'src/environment/environment';
-import { User } from '../interfaces/user';
+import { RestService } from './rest.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService extends RestService{
   endpoint = `${environment.api}/users`
-
-  constructor(private http: HttpClient) { }
-
-  all(page: number): Observable<any> {
-    return this.http.get<any>(`${this.endpoint}?page=${page}`);
-  }
-
-  create(data: any): Observable<User> {
-    return this.http.post<User>(this.endpoint, data);
-  }
-
-  get(id: number): Observable<User> {
-    return this.http.get<User>(`${this.endpoint}/${id}`);
-  }
-
-  update(id: number, data: User): Observable<User> {
-    return this.http.put<User>(`${this.endpoint}/${id}`, data)
-  }
-
-  delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.endpoint}/${id}`)
-  }
 }
