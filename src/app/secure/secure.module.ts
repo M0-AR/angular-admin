0d90830062bdab87d10b5c76cb5c704adbc2f8dd
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-01-19 16:09:33 +0100

    Roles

--- src/app/secure/secure.module.ts
+++ src/app/secure/secure.module.ts
@@ -10,6 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UsersComponent } from './users/users.component';
 import { UserCreateComponent } from './users/user-create/user-create.component';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
+import { RolesComponent } from './roles/roles.component';
 
 
 
@@ -22,7 +23,8 @@ import { UserEditComponent } from './users/user-edit/user-edit.component';
     DashboardComponent,
     UsersComponent,
     UserCreateComponent,
-    UserEditComponent
+    UserEditComponent,
+    RolesComponent
   ],
   imports: [
     CommonModule,
